8eb7594a70731decdb56dd628053a1b371fdc1d0
aileiys <zysongl@gmail.com>
2019-02-19 22:20:59 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+mkdir /etc/snell/
 apt-get install unzip
-cd /usr/bin/
+cd /usr/local/bin/
 wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/1.0.1/snell-server-v1.0.1-linux-amd64.zip
 unzip snell.zip
 rm -f snell.zip
@@ -15,14 +16,16 @@ Description=Snell Server
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/snell-server
+ExecStart=/usr/local/bin/snell-server -c /etc/snell/snell-server.conf
 Restart=on-failure
 RestartSec=1s
 
 [Install]
 WantedBy=multi-user.target
 EOF
+
 systemctl daemon-reload
 systemctl start snell
 systemctl restart snell
+
 cat /etc/snell/snell-server.conf
