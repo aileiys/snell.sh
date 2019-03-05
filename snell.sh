067075654cbb5c5619af6f33312374684a59168a
aileiys <zysongl@gmail.com>
2019-03-05 16:26:39 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -4,7 +4,7 @@ export PATH
 mkdir /etc/snell/
 apt-get install unzip
 cd /usr/local/bin/
-wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/1.0.1/snell-server-v1.0.1-linux-amd64.zip
+wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/v1.1.0/snell-server-v1.1.0-linux-amd64.zip
 unzip snell.zip
 rm -f snell.zip
 chmod +x snell-server
